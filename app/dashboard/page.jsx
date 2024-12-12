dd2c543949ecc9f04d967394f0a1193b17ad4e73
Heat Koemnak <heatkimnak@gmail.com>
2024-12-13 03:47:01 +0700

    new updated

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -100,19 +100,14 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      {cookiesVisible ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {posts.map((post, index) =>
-            post?.authorEmail === session?.user.email ? (
-              <Post key={index} post={post} />
-            ) : (
-              'No post created yet.'
-            )
-          )}
-        </div>
-      ) : (
-        <div>Redirecting to sign in...</div>
-      )}
+      {cookiesVisible &&
+        posts?.map((post, index) =>
+          post?.authorEmail === session?.user.email ? (
+            <Post key={index} post={post} />
+          ) : (
+            'Redirecting to sign in...'
+          )
+        )}
     </div>
   );
 }
