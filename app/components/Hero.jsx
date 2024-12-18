68d1689bc560de1facdad6f57ebab1c7c7def082
Heat Koemnak <heatkimnak@gmail.com>
2024-12-19 01:02:41 +0700

    updated

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <AuthButton />
         </div> */}
         {/* <Chat /> */}
-        <div className="max-w-lg px-4 sm:pt-24 pt-12  mx-auto text-left md:max-w-none md:text-center">
+        <div className="max-w-lg px-4 mx-auto text-left md:max-w-none md:text-center">
           <div className="text-center py-4 hidden sm:block">
             <button className="bg-white border border-[#E2E8F0] hover:bg-neutral-200 text-xs font-bold py-2.5 px-4 rounded-full inline-flex items-center">
               <svg
@@ -29,21 +29,68 @@ const Hero = () => {
               &nbsp; &nbsp;<span> Follow us on Twitter </span>
             </button>
           </div>
-          <h1 className="font-extrabold  text-[#201515] text-center sm:leading-none text-5xl sm:text-9xl">
-            <span className="inline md:block ">
-              Bring Your Ideas{' '}
-            </span>
-            <span className="relative mt-2 bg-clip-text text-[#201515] md:inline-block">
-              to Life
-            </span>
-            <span className="relative mt-2 bg-clip-text text-[#FF4F01]">.</span>
-          </h1>
-          {/* <p className=" text-center mt-4 font-base lg:text-md max-w-4xl mx-auto  text-gray-600">
-            Whether {`you're`} a creator, innovator, or dreamer, our platform is
-            designed to help you share your ideas with the world.
-          </p> */}
+
+          <section class="bg-center lg:max-w-9xl mx-auto h-[70vh] bg-no-repeat  bg-[url('https://i.pinimg.com/originals/2e/2b/21/2e2b21aeed393403d4620367f9e093f9.gif')] bg-gray-700 bg-blend-multiply">
+            <div class="px-4 mx-auto max-w-screen-xl text-center py-20">
+              <h1 className="tracking-tight mb-5 leading-none font-extrabold text-[#FF4F01] text-center sm:leading-none text-4xl sm:text-5xl">
+                <span className="inline md:block ">Bring Your Ideas </span>
+                <span className="relative mt-2 bg-clip-text text-white md:inline-block">
+                  to Life
+                </span>
+                <span className="relative mt-2 bg-clip-text text-[#FF4F01]">
+                  .
+                </span>
+              </h1>
+              {/* <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                We invest in the world’s potential
+              </h1> */}
+              {/* <h1 className="hidden lg:block font-extrabold text-[#FF4F01] text-center sm:leading-none text-5xl sm:text-9xl">
+                <span className="inline md:block ">Bring Your Ideas </span>
+                <span className="relative mt-2 bg-clip-text text-white md:inline-block">
+                  to Life
+                </span>
+                <span className="relative mt-2 bg-clip-text text-[#FF4F01]">
+                  .
+                </span>
+              </h1> */}
+              <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+                Whether {`you're`} a creator, innovator, or dreamer, our
+                platform is designed to help you share your ideas with the
+                world.
+              </p>
+              <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                <a
+                  href="#"
+                  class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                >
+                  Get started
+                  <svg
+                    class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+                <a
+                  href="#"
+                  class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </section>
         </div>
-        <div className="max-w-lg px-4 mx-auto flex justify-center text-center md:text-center">
+        {/* <div className="max-w-lg px-4 mx-auto flex justify-center text-center md:text-center">
           <div className="flex text-center items-center lg:py-6 py-8 my-4 space-x-2  ">
             <button className="backdrop-blur-sm transition duration-500 ease-in-out bg-[#FF4F01] border border-[#E2E8F0] translate-y-1 text-white hover:bg-orange-200 text-md font-medium lg:py-3 lg:px-6 px-4 py-1 rounded-full inline-flex items-center">
               <span> Get Started</span>
@@ -55,18 +102,10 @@ const Hero = () => {
               <span> Explore Features</span>
             </Link>
           </div>
-        </div>
+        </div> */}
       </section>
-      <div className="flex lg:flex-row flex-col p-10 rounded-3xl lg:my-20 my-5 justify-between items-center gap-10 bg-gradient-to-r from-red-200 to-orange-200 ">
-        <div className=" ">
-          <Image
-            className="object-cover rounded-3xl object-center w-full mx-auto bg-gray-300 lg:ml-auto"
-            alt="hero"
-            width={600}
-            height={600}
-            src="https://i.pinimg.com/originals/2e/2b/21/2e2b21aeed393403d4620367f9e093f9.gif"
-          />
-        </div>
+      <div className="flex border-b-2 lg:flex-row flex-col p-10 rounded-3xl lg:my-20 my-5 justify-between items-center gap-10">
+      
         <div className=" items-center">
           <div className=" text-center lg:text-left">
             <div>
--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -4,8 +4,14 @@ import React from 'react';
 const Logo = () => {
   return (
     <Link href={'/'}>
-      <h1 className="text-3xl xs:text-2xl sm:text-2xl lg:text-3xl xl:text-3xl bg-gradient-to-r from-orange-400 font-black to-purple-500 bg-clip-text text-transparent">
+      {/* <h1 className="text-3xl xs:text-2xl sm:text-2xl lg:text-3xl xl:text-3xl bg-gradient-to-r from-orange-400 font-black to-purple-500 bg-clip-text text-transparent">
         Power.ME
+      </h1> */}
+      <h1 className="font-extrabold  text-[#201515] text-center sm:leading-none text-2xl ">
+        <span className="relative  bg-clip-text text-[#201515] md:inline-block">
+          PME
+        </span>
+        <span className="relative mt-2 bg-clip-text text-[#FF4F01]">.</span>
       </h1>
       {/* <h1 className="text-3xl xs:text-2xl sm:text-2xl lg:text-3xl xl:text-3xl bg-orange-500 font-extrabold bg-clip-text text-transparent">
         Power.ME
