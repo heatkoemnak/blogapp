4dbd90445c71088552d93f2e302ad9460610bfd5
Heat Koemnak <heatkimnak@gmail.com>
2024-12-23 14:58:13 +0700

    updated

--- app/components/CreatePostForm.jsx
+++ app/components/CreatePostForm.jsx
@@ -300,7 +300,7 @@ const CreatePostForm = () => {
                 type="submit"
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {isLoading ? <LoadingSpinner /> : 'Create'}
+                {loading ? <LoadingSpinner /> : 'Create'}
               </button>
             </div>
           </div>
