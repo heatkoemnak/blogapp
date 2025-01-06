a0790b6f049f5cb69e4c90227690583cc894f094
Heat Koemnak <heatkimnak@gmail.com>
2025-01-06 13:47:19 +0700

    updated

--- app/create-post/page.jsx
+++ app/create-post/page.jsx
@@ -1,22 +1,15 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSession, signIn } from 'next-auth/react';
-import CreatePostForm from '../components/CreatePostForm';
-import Post from '../components/Post';
-import { PreviewModal } from '../components/ui/modals/PreviewModal';
 import FormPostJob from '../components/FormPostJob';
 const CreatePost = () => {
-  // const { data: session, status } = useSession();
-  // console.log(session, status);
-  // const [cookiesVisible, setCookiesVisible] = useState(false);
+  const { data: session, status } = useSession();
 
-  // useEffect(() => {
-  //   if (!session) {
-  //     signIn();
-  //   }
-  //   const timer = setTimeout(() => setCookiesVisible(true), 1500);
-  //   return () => clearTimeout(timer);
-  // }, [session]);
+  useEffect(() => {
+    if (!session) {
+      signIn();
+    }
+  }, [session]);
 
   return (
     <div className="max-full h-auto mx-auto ">
