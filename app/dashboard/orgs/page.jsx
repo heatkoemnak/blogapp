c98b5cbc34d48813f7bbeb3726760a32c613caed
Heat Koemnak <heatkimnak@gmail.com>
2025-06-18 23:02:26 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "updated"1"2"3"4"5"6"
    This reverts commit 345462ed0cb19f2a2251870a125528db9fa08536.

--- app/dashboard/orgs/page.jsx
+++ app/dashboard/orgs/page.jsx
@@ -316,7 +316,7 @@ const handleCustomExport = () => {
                       className="font-medium text-red-600 hover:underline"
                     >
                       <DeleteCompanyPopup
-                        companyName={company?.name}
+                        compnayName={company?.name}
                         companyId={company?.id}
                         handleDelete={() => handleDelete(company.id)}
                         error={error}
